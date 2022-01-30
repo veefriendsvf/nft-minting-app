8c58fa5e9930e55bce23c4354e88b940a11f3ccb
veefriendsvf <98550050+veefriendsvf@users.noreply.github.com>
2022-01-30 23:05:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -243,7 +243,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {data.totalSupply} / {CONFIG.MAX_SUPPLY}
+              {data.totalCountDone} / {CONFIG.MAX_SUPPLY}
             </s.TextTitle>
             <s.TextDescription
               style={{
