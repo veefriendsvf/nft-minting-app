1d79137d7f0b4cfc17ddf7dc34ebf8e458adc89a
veefriendsvf <98550050+veefriendsvf@users.noreply.github.com>
2022-02-05 11:57:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export const ResponsiveWrapper = styled.div`
   flex-direction: column;
   justify-content: stretched;
   align-items: stretched;
-  width: 80%;
+  width: 73%;
   @media (min-width: 767px) {
     flex-direction: row;
   }
